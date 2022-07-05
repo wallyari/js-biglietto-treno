d2c2d4aec2f0a52c51c2ad3da814090c66611a99
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-05 15:15:10 +0200

    Correzione Js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const userKm = parseInt( prompt ('Inserisci il numero di chilometri che devi per
 console.log (userKm);
 
 //ETÁ DEL PASSEGGERO//
-const userEtà = parseInt( prompt ('Inserisci l\'età del passeggero'));
+const userEta = parseInt( prompt ('Inserisci l\'età del passeggero'));
 console.log (userEtà)
 
 
@@ -12,11 +12,11 @@ const prezzoIniziale = userKm * 0.21;
 let prezzoTotale;
 
 //SCONTI IN BASE ALL'ETÁ//
-if (userEtà < 18){
+if (userEta < 18){
     prezzoTotale = prezzoIniziale - prezzoIniziale * 0.2;
 }
 
-if (userEtà > 65) {
+if (userEta > 65) {
     prezzoTotale = prezzoIniziale - prezzoIniziale  * 0.4;
 }
 else{
